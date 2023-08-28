2212fa80790a8fb2a2df716e6541a5f8cb191e9a
Filipe Magalhães Lima <97008929+FilipeMagal@users.noreply.github.com>
2023-08-27 23:33:35 -0300

    Update Conexao.java
    Conexão atualizada

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -5,23 +5,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:mysql://localhost:3306/cadastro_pessoas";
-    private static final String user = "root";
-    private static final String password = "";
+    // Configurações para estabelecer a conexão com o banco de dados
+    private static final String url = "jdbc:mysql://localhost:3306/cadastro_pessoas";// URL do banco de dados
+    private static final String user = "root";// Nome de usuário do banco de dados
+    private static final String password = "";// Senha do banco de dados
 
-    private static Connection conn;
+    private static Connection conn;// Variável para armazenar a conexão
 
-    public static Connection getConn() {
+    public static Connection getConn() {  // Método estático para obter uma conexão com o banco de dados
         try {
-            if (conn == null) {
-                conn = DriverManager.getConnection(url, user, password);
-                return conn;
+            if (conn == null) { // Se ainda não existe uma conexão
+                conn = DriverManager.getConnection(url, user, password);// Estabelece a conexão
+                return conn;// Retorna a conexão estabelecida
             } else {
-                return conn;
+                return conn;// Retorna a conexão existente
             }
         } catch(SQLException e){
-            e.printStackTrace();
-            return null;
+            e.printStackTrace();// Imprime informações sobre o erro no console
+            return null;// Retorna null em caso de erro
         }
     }
 }
